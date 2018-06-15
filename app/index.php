e4c3bb13af32f552524a5b9e10fca44a82c5d449
rhe24 <rhys301097@gmail.com>
2018-06-15 15:28:31 +0100

    Added intro section

--- app/index.php
+++ app/index.php
@@ -54,9 +54,72 @@
         </div>
       </nav>
     </header>
-    <footer>
+    <!-- Intro -->
+    <section class="page-section clearfix">
+      <div class="container">
+        <div class="intro text-white">
+          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/portrait.jpg" alt="">
+          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Introduction</span>
+              <span class="section-heading-lower">Welcome</span>
+            </h2>
+            <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            </p>
+            <div class="intro-button mx-auto">
+              <a class="btn btn-primary btn-lg" href="#">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Links -->
+    <section class="bg-light">
+
+    </section>
+    <!-- Footer -->
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <p class="copyright" id="copyright">&copy; Copyright <span id="date"></span> John Doe</p>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a href="">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="">
+                  <i class="fa fa-youtube"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="" >
+                  <i class="fa fa-linkedin"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline quicklinks">
+              <li class="list-inline-item">
+                Design by <a href="http://rhysevans.xyz">Rhys Evans</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
 
+    <!-- Javascript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -64,6 +127,6 @@
     <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
     <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
-    <script src="js/rhysevans.min.js"></script>
+    <script src="js/portfolio-site.min.js"></script>
   </body>
 </html>
---
+++ app/js/portfolio-site.js
@@ -0,0 +1,6 @@
+(function($){  // Update the date of copyright notice
+  "use strict";
+
+  var d = new Date();
+  document.getElementById("date").innerHTML=d.getFullYear();
+})(jQuery);
